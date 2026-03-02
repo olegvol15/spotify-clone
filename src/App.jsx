c370d42adc9acbe2fb1d902b874124ba9952aae2
TamerlanRL <106627882+TamerlanRL@users.noreply.github.com>
2026-03-02 17:12:05 +0200

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Header from "./components/Header";
+import NotFound from "./components/NotFound";
+
+export default function App() {
+  return (
+    <div className="app">
+      <Header />
+      <NotFound />
+    </div>
+  );
+}
